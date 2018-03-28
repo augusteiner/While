7daa6be8e451ae6109356fe72ea7b917d7a33f36
José Nascimento <joseaugustodearaujonascimento@gmail.com>
2018-03-28 08:34:56 -0300

    suporte a versões do java < 8
    - remove tabs

--- src/main/java/plp/enquanto/Principal.java
+++ src/main/java/plp/enquanto/Principal.java
@@ -15,26 +15,26 @@ import plp.enquanto.parser.MeuListener;
 
 public class Principal {
 
-	private static ParseTree parse(String arq) {
-		CharStream input;
-		try {
-			input = CharStreams.fromFileName(arq);
-		} catch (IOException e) {
-			input = CharStreams.fromString("exiba \"Arquivo não encontrado.\"");
-		}
-		final EnquantoLexer lexer = new EnquantoLexer(input);
-		final CommonTokenStream tokens = new CommonTokenStream(lexer);
-		final EnquantoParser parser = new EnquantoParser(tokens);
-		return parser.programa();
-	}
+    private static ParseTree parse(String arq) {
+        CharStream input;
+        try {
+            input = CharStreams.fromFileName(arq);
+        } catch (IOException e) {
+            input = CharStreams.fromString("exiba \"Arquivo não encontrado.\"");
+        }
+        final EnquantoLexer lexer = new EnquantoLexer(input);
+        final CommonTokenStream tokens = new CommonTokenStream(lexer);
+        final EnquantoParser parser = new EnquantoParser(tokens);
+        return parser.programa();
+    }
 
-	public static void main(String... args) throws IOException {
-		String arq = args[0];
-		final ParseTree tree = parse(arq);
-		final ParseTreeWalker walker = new ParseTreeWalker();
-		final MeuListener listener = new MeuListener();
-		walker.walk(listener, tree);
-		Programa p1 = listener.getPrograma();
-		p1.execute();
-	}
+    public static void main(String... args) throws IOException {
+        String arq = args[0];
+        final ParseTree tree = parse(arq);
+        final ParseTreeWalker walker = new ParseTreeWalker();
+        final MeuListener listener = new MeuListener();
+        walker.walk(listener, tree);
+        Programa p1 = listener.getPrograma();
+        p1.execute();
+    }
 }
